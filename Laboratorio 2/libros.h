75e427ef596c78ea50a4a0ee66191af569c1c25d
jerimengonzalez21 <jerimengonzalez21@gmail.com>
2025-10-23 17:16:22 -0300

    Update libros.h
    new

--- Laboratorio 2/libros.h
+++ Laboratorio 2/libros.h
@@ -1,8 +1,12 @@
 #ifndef LIBROS_H
 #define LIBROS_H
-
-#include "librosysocios.h"
-
+struct Libro {
+int id;
+char titulo[30];
+char autor[50];
+int anio;
+int cantidad;
+};
 extern struct Libro *libros;
 extern int cantidad_libros;
 
