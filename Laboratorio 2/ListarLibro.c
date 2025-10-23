75fbdc3c1059cbb5b305257a6367a4be068427e2
jerimengonzalez21 <jerimengonzalez21@gmail.com>
2025-10-23 17:12:45 -0300

    Update ListarLibro.c
    codigo nuevo

--- Laboratorio 2/ListarLibro.c
+++ Laboratorio 2/ListarLibro.c
@@ -2,11 +2,7 @@
 #include <stdlib.h>
 #include "libros.h"
 
-int cantidad_libros = 10000;
-
-struct Libro *libros = NULL;
-
-void inicializar_libros() {
+void inicializar_libros(struct Libro *libros, int cantidad_libros) {
     libros = malloc(cantidad_libros * sizeof(struct Libro));
 
     if (libros == NULL){
@@ -21,14 +17,3 @@ void inicializar_libros() {
     libros[4] = (struct Libro){5,"1984", "George Orwell",1949,4};
 }
 
-int main(){
-    inicializar_libros();
-    for (int i = 0; i < libros[i].id; i++){
-    printf("------------------------------\n");
-    printf(" ID: %d\n Titulo: %s\n Autor: %s\n Año: %d\n Cantidad: %d\n", 
-        libros[i].id, libros[i].titulo, libros[i].autor, libros[i].anio, libros[i].cantidad);
-    } // Aquí podrías agregar código para probar la funcionalidad de los libros.
-    printf("------------------------------\n");
-    free(libros);
-    return 0;
-}
