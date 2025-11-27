b82a9ad0fb57dda0f9abefcc0a339d710ab0f396
jerimengonzalez21 <jerimengonzalez21@gmail.com>
2025-11-27 14:25:53 -0300

    Update socios.h

--- Trabajo Practico ejercicios/socios.h
+++ Trabajo Practico ejercicios/socios.h
@@ -1,1 +1,17 @@
+#ifndef SOCIOS_H
+#define SOCIOS_H
 
+#define MAX_TEXTO 50
+#define MAX_SOCIOS 100
+
+struct Socio {
+    int id;
+    char nombre[MAX_TEXTO];
+    char correo[MAX_TEXTO];
+};
+
+void agregarSocio(struct Socio socios[], int *cantidad);
+ 
+void listarSocios(struct Socio socios[], int cantidad);
+
+#endif
