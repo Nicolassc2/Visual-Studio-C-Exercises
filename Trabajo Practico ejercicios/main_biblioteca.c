0930611667677aa2881599a36ea70d361a041101
jerimengonzalez21 <jerimengonzalez21@gmail.com>
2025-11-27 14:22:54 -0300

    Update main_biblioteca.c

--- Trabajo Practico ejercicios/main_biblioteca.c
+++ Trabajo Practico ejercicios/main_biblioteca.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include "librosysocios.h"
+#include "listar_libros.h"
 
+int main() {
+    struct Libro libros[100];
+    int cantidad_Libros = 10000;
+    int opcion;
+
+    do {
+        printf("\n--- MENU ---\n");
+        printf("1. Registrar libro\n");
+        printf("2. Listar libros\n");
+        printf("0. Salir\n");
+        printf("Opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+               // registrarLibro(libros, &cantidad_Libros);
+                break;
+            case 2:
+             inicializar_libros();
+                break;
+        }
+    } while (opcion != 0);
+
+    return 0;
+    return 0;
+}
